ca0a21676d58e12b937349457d34214bdf93bd47
Paweł <jembezmamy@users.noreply.github.com>
2017-09-25 15:52:13 +0200

    header

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,11 @@
-// This is where it all goes :)
+$(function() {
+  var $header = $("[data-component=header]");
+  var $window = $(window);
+  $window.on("scroll", function() {
+    if ($window.scrollTop() > 5) {
+      $header.addClass("visible");
+    } else {
+      $header.removeClass("visible");
+    }
+  })
+});
