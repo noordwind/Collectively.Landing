4c26efff19fe24d15938bedb58c15b406a38d522
Paweł <jembezmamy@users.noreply.github.com>
2017-09-26 16:07:33 +0200

    contact form js

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -30,4 +30,68 @@ $(function() {
       $(el).attr("src", $(el).data("2x-src"));
     });
   }
+
+
+  // contact form
+  $("[data-component=contact-form]").on("submit", function(e) {
+    e.preventDefault();
+
+    var $form = $(e.currentTarget);
+    var $inputs = $form.find("input, textarea");
+    var translations = $form.data("translations");
+
+    $inputs.on("change keyup", function(e) {
+      $(e.currentTarget).closest(".input").removeClass("with-errors");
+    });
+
+    function findInput(attr) {
+       return $inputs.filter("[name=" + attr + "]");
+    }
+
+    function addError(attr, error) {
+      valid = false;
+      var $el = findInput(attr).closest(".input");
+      $el.addClass("with-errors");
+      $el.find(".errors").text(error);
+    }
+
+    function setState(state) {
+      $form.removeClass("state-loading state-success state-failure");
+      if (state) {
+        $form.addClass("state-" + state);
+      }
+    }
+
+    var data = {
+      name: $.trim(findInput("name").val()),
+      email: $.trim(findInput("email").val()),
+      message: $.trim(findInput("message").val())
+    };
+
+    var valid = true;
+
+    if (!data.email || data.email.length === 0) {
+      addError("email", translations.presence);
+    } else if (!data.email.match(/^[^@]+@[^@]+$/)) {
+      addError("email", translations.email);
+    }
+    if (!data.message || data.message.length === 0) {
+      addError("message", translations.presence);
+    }
+
+    if (valid) {
+      $inputs.attr("disabled", true);
+      setState("loading");
+      $.post($form.attr("action"), data, function() {
+        setState("success");
+        $inputs.val("");
+        setTimeout(setState, 5000);
+      }).fail(function() {
+        setState("failure");
+        setTimeout(setState, 5000);
+      }).always(function() {
+        $inputs.attr("disabled", false);
+      });
+    }
+  });
 });
